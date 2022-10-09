bfff51536e220de46db7d7c674163c4f7edc6c11
Yu Bai <43456665+watermantle@users.noreply.github.com>
2022-10-09 09:52:50 -0400

    Saved helper function in util.py
    Separate helper function text_parse to file util.py

--- HW2/Question1_part2.py
+++ HW2/Question1_part2.py
@@ -1,16 +1,7 @@
-# parse function to check if a website has a specific target word
-def text_parse(soup, target_word):
-    pattern = f'.*?({target_word}).*?'
-    for p in soup.find_all('p'):
-        text = p.get_text()
-        text = text.strip().lower()
-        res = re.findall(pattern, text, re.S)
-        if len(res) != 0: return True
-    return False
-
 # program to crawl 20 urls that contain key word "charges"
 import urllib.request
 import re
+from util import text_parse
 from bs4 import BeautifulSoup
 
 
